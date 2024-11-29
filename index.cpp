f44f1892bde2a73d773c0791750303b46cb7b983
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2024-11-29 14:05:34 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 3;
+    int n1,n2,n3;
+    cout<<"Enter the number:";
+    cin>>n1;
+     cout<<"Enter the number:";
+    cin>>n2;
+     cout<<"Enter the number:";
+    cin>>n3;
+    
+    if(n = 3){
+       cout<<n1+n2+n3;  
+    }
+   
+}
